e43c214336fa2ae53d63fc910bb1618f42dcc52d
Aiden Egglin <uefadude@gmail.com>
2025-06-09 05:38:26 -0400

    Use threading for window

--- Sapling_cpp/main.cpp
+++ Sapling_cpp/main.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
+#include <thread>
 #include <SFML/Graphics.hpp>
+
+void renderingThread(sf::RenderWindow* window)
+{
+	window->setActive(true);
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window->isOpen())
+	{
+		window->clear(sf::Color::Black); // Calling clear before drawing anything is mandatory
+		window->draw(shape);
+		window->display(); //takes what was drawn on the last call to display and displays it
+	}
+}
+
 
 int main()
 {
-
+	// create window in main thread
 	sf::RenderWindow window(sf::VideoMode({ 600, 600 }), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+
+
+	// deactivate its OpenGL context
+	window.setActive(false);
+
+	// launch the rendering thread
+	std::thread thread(&renderingThread, &window);
 
 	while (window.isOpen()) {
 		while (const std::optional event = window.pollEvent()) {
 			if (event->is<sf::Event::Closed>())
 				window.close();
 		}
-		window.clear();
-		window.draw(shape);
-		window.display();
 	}
-
+	thread.join();
 	//std::cin.get();
 }
