3724bcffe78721edd807318fa1cc6aaa188ff896
Aiden Egglin <uefadude@gmail.com>
2025-06-10 06:06:58 -0400

    Start adding key handling

--- Sapling_cpp/main.cpp
+++ Sapling_cpp/main.cpp
@@ -20,19 +20,28 @@ void renderingThread(sf::RenderWindow* window)
 int main()
 {
 	// create window in main thread
-	sf::RenderWindow window(sf::VideoMode({ 600, 600 }), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode({ 600, 600 }), "SFML works!", sf::Style::Close, sf::State::Windowed);
 
 
 	// deactivate its OpenGL context
 	window.setActive(false);
+	// Be sure application refresh rate is synchronized with vertical freq of monitor to avoid tearing
+	//window.setVerticalSyncEnabled(true);
+
+	window.setFramerateLimit(60); // set the frame limit explicitly
 
 	// launch the rendering thread
 	std::thread thread(&renderingThread, &window);
 
 	while (window.isOpen()) {
 		while (const std::optional event = window.pollEvent()) {
-			if (event->is<sf::Event::Closed>())
+			if (event->is<sf::Event::Closed>()) {
 				window.close();
+			}
+			else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+				if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+					window.close();
+			}
 		}
 	}
 	thread.join();
